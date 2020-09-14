cf614426df2bafab6b4a5a59ba481fe44f33078e
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2020-09-14 20:08:23 -0300

    Project created

---
+++ backend/src/main/java/com/devnaldo/dnpesquisa/DnpesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devnaldo.dnpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnpesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnpesquisaApplication.class, args);
+	}
+
+}
