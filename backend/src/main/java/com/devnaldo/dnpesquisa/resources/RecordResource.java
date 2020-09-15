623c4290710411be2b88a90ef4f4fb2b887cabea
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2020-09-15 17:10:45 -0300

    GET records

--- backend/src/main/java/com/devnaldo/dnpesquisa/resources/RecordResource.java
+++ backend/src/main/java/com/devnaldo/dnpesquisa/resources/RecordResource.java
@@ -1,12 +1,21 @@
 package com.devnaldo.dnpesquisa.resources;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devnaldo.dnpesquisa.dto.GameDTO;
 import com.devnaldo.dnpesquisa.dto.RecordDTO;
 import com.devnaldo.dnpesquisa.dto.RecordInsertDTO;
 import com.devnaldo.dnpesquisa.services.RecordService;
@@ -21,5 +30,25 @@ public class RecordResource {
     public ResponseEntity<RecordDTO> insert (@RequestBody RecordInsertDTO dto) {
      RecordDTO newDTO = service.insert(dto);
      return ResponseEntity.ok().body(newDTO);
+    }
+    @GetMapping
+    public ResponseEntity <Page<RecordDTO>> findAll (
+    		@RequestParam(value = "mim", defaultValue = "") String mim,
+    		@RequestParam(value = "max", defaultValue = "") String max,
+    		@RequestParam(value = "page", defaultValue = "0") Integer page,
+    		@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+    		@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+    		@RequestParam(value = "direction", defaultValue = "DESC") String direction){
+    	
+    	Instant minDate = ("".equals(mim)) ? null : Instant.parse(mim);
+    	Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+    	
+    	if(linesPerPage == 0) {
+    		linesPerPage = Integer.MAX_VALUE;
+    	}
+    	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+    	
+    	Page <RecordDTO> list = service.findByMoments(minDate, maxDate, pageRequest);
+    	return ResponseEntity.ok().body(list);
     }
 }
