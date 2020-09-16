efdce29bb47c60bc40e170a50c9307c3b807d2a0
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2020-09-16 20:50:55 -0300

    Criando página Home

---
+++ front-web/src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ReactComponent as SetaIcon } from '../../assets/Seta.svg'
+import { ReactComponent as GameImage } from '../../assets/GAMER.svg'
+import './style.css';
+
+
+const Home = () => {
+    return (
+      <div className="home-container">
+          <div className="home-text">
+              <h1 className="home-text-title">
+              Quais jogos a galera gosta mais?
+              </h1>
+              <h3 className="home-text-subtitle">
+              Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!
+              </h3>
+              <div className="home-actions">
+                  <button className="home-btn">
+                  QUERO SABER QUAIS SÃO
+                  </button>
+                  <div className="home-btn-icon">
+                      <SetaIcon/>
+                  </div>
+              </div>
+          </div>
+          <GameImage className="home-image"/>
+      </div> 
+
+    );
+}
+
+export default Home;
